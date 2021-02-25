1673131ca7d4c3eb823cb69bb1d6f8123d9c70f5
cgorka <cgorka@gmail.com>
2021-02-26 00:23:39 +0100

    t

--- src/Box.js
+++ src/Box.js
@@ -6,7 +6,7 @@ const Box = (props) => {
         <div className="box" key={props.item.first_name}>
             {/*<img src={props.item.image+'?r='+props.item.id }  width="50" height="50"alt=""/>*/}
 
-            <p class='id'> {props.item.id}</p>
+            {/*<p class='id'> {props.item.id}</p>*/}
             <p class='name'> {props.item.first_name}</p>
             <p class='product'> {props.item.genre}</p>
             <p class='mass'> {props.item.mass}</p>
