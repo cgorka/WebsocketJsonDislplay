dd709419da6cfa93330100971b13fdf365a4150a
cgorka <cgorka@gmail.com>
2021-02-27 15:46:23 +0100

    t

--- src/Box.js
+++ src/Box.js
@@ -1,7 +1,9 @@
 import React, { PropTypes } from "react";
+import { ReactComponent as Hy10svg } from './images/hy10.svg';
 
 const Box = (props) => {
   // console.log('props: ', props);
+
   var sizeInPx = "200px";
   if (props.size === "big") {
     sizeInPx = "300px";
@@ -16,6 +18,12 @@ const Box = (props) => {
       key={props.item.first_name}
       style={{ width: sizeInPx, height: sizeInPx }}
     >
+      {/*<Hy10svg fill='red' stroke='white'/>*/}
+
+      <div style={{height:'50px',
+        width: '50px'}} className="size">
+        <Hy10svg stroke='white'/>
+      </div>
       <p class="name"> {props.item.first_name}</p>
       <p class="product"> {props.item.genre}</p>
       <p class="mass"> {props.item.mass}</p>
