76739855f9db32fd52a354a400b40269e2206925
cgorka <cgorka@radwag.pl>
2021-03-01 16:11:23 +0100

    1

--- src/BoxFullScrean.js
+++ src/BoxFullScrean.js
@@ -1,22 +1,40 @@
-import React, {PropTypes} from 'react'
+import React, { PropTypes } from "react";
 import Box from "./Box";
-import { useHistory,useParams } from "react-router-dom";
+import { ReactComponent as Hy10svg } from "./images/hy10.svg";
+import { useHistory, useParams } from "react-router-dom";
 
 const BoxFullScrean = (props) => {
-    let history = useHistory();
-    let { id } = useParams();
+  let history = useHistory();
+  let { id } = useParams();
 
-    function clickHandled(){
-        history.push("/")
-    }
-    return (
-        <div className='containerFullBox'>
-            {/*<button onClick={clickHandled}>close</button>*/}
-            <h1>Box Full</h1>
-            <p>test data{id}</p>
-            {/*<Box></Box>*/}
-        </div>
-    );
+  function clickHandled() {
+    history.push("/");
+  }
+  return (
+    <div className="containerFullBox grid-container">
+      {/*<button onClick={clickHandled}>close</button>*/}
+      {/*<h1>Box Full</h1>*/}
+      {/*<p>test data{id}</p>*/}
+
+      <div className="Grid-Hader">
+        <button>Close</button>
+      </div>
+      <div className="Icon">
+        {/*<Hy10svg stroke={props.item.status}/>*/}
+        <Hy10svg className="ScaleSVG" stroke="green" />
+      </div>
+      <div className="Grid-Footer"></div>
+      <div className="Data1"></div>
+      <div className="Data2"></div>
+      <div className="Data3"></div>
+      <div className="Data4"></div>
+      <div className="Data5"></div>
+      <div className="Data6"></div>
+      <div className="Icon2"></div>
+
+      {/*<Box></Box>*/}
+    </div>
+  );
 };
 
 BoxFullScrean.propTypes = {};
